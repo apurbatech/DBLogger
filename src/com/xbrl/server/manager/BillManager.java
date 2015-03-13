306da03cdc1d2dbae4fdca20724ba2d3eadb96ad
nazmul hasan <nazmul@apurbatech.com>
2015-03-13 17:55:58 +0600

    Bill module is added

---
+++ src/com/xbrl/server/manager/BillManager.java
@@ -0,0 +1,23 @@
+package com.xbrl.server.manager;
+
+import com.xbrl.server.bean.BillInfo;
+
+/**
+ *
+ * @author nazmul hasan
+ */
+public class BillManager extends DBConnectionManager{
+    public BillManager()
+    {
+    
+    }
+    
+    /*
+     * This method will store bill info
+     * @author nazmul hasan
+     */
+    public void storeBillInfo(BillInfo billInfo)
+    {
+        
+    }
+}
